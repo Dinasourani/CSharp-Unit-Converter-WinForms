b147a7107a1524f22077c154a577a678f357ba14
Dina sourani <dyxz8613@gmail.com>
2025-11-28 18:20:17 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Location = new System.Drawing.Point(289, 137);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTo.TabIndex = 0;
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(289, 28);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(121, 24);
+            this.cmbType.TabIndex = 1;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(65, 28);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(100, 22);
+            this.txtValue.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(67, 77);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(46, 17);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "label1";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(70, 116);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Location = new System.Drawing.Point(289, 77);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(121, 24);
+            this.cmbFrom.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Result :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "From";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "To";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 173);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbFrom);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.cmbTo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // نوع کمیت
+            cmbType.Items.Add("طول");
+            cmbType.Items.Add("جرم");
+            cmbType.Items.Add("زمان");
+            cmbType.Items.Add("دما");
+
+            cmbType.SelectedIndexChanged += CmbType_SelectedIndexChanged;
+            btnConvert.Click += BtnConvert_Click;
+        }
+
+        private void CmbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbFrom.Items.Clear();
+            cmbTo.Items.Clear();
+
+            string type = cmbType.SelectedItem.ToString();
+
+            if (type == "طول")
+            {
+                cmbFrom.Items.AddRange(new object[] { "m", "cm", "mm", "km", "ft", "in" });
+                cmbTo.Items.AddRange(new object[] { "m", "cm", "mm", "km", "ft", "in" });
+            }
+            else if (type == "جرم")
+            {
+                cmbFrom.Items.AddRange(new object[] { "kg", "g", "lb" });
+                cmbTo.Items.AddRange(new object[] { "kg", "g", "lb" });
+            }
+            else if (type == "زمان")
+            {
+                cmbFrom.Items.AddRange(new object[] { "s", "min", "h" });
+                cmbTo.Items.AddRange(new object[] { "s", "min", "h" });
+            }
+            else if (type == "دما")
+            {
+                cmbFrom.Items.AddRange(new object[] { "C", "F", "K" });
+                cmbTo.Items.AddRange(new object[] { "C", "F", "K" });
+            }
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = double.Parse(txtValue.Text);
+                string from = cmbFrom.SelectedItem.ToString();
+                string to = cmbTo.SelectedItem.ToString();
+                string type = cmbType.SelectedItem.ToString();
+                double result = 0;
+
+                if (type == "دما")
+                {
+                    // تبدیل دما
+                    if (from == "C")
+                        result = (to == "F") ? value * 9 / 5 + 32 : (to == "K") ? value + 273.15 : value;
+                    else if (from == "F")
+                        result = (to == "C") ? (value - 32) * 5 / 9 : (to == "K") ? (value - 32) * 5 / 9 + 273.15 : value;
+                    else // from == "K"
+                        result = (to == "C") ? value - 273.15 : (to == "F") ? (value - 273.15) * 9 / 5 + 32 : value;
+                }
+                else
+                {
+                    // تبدیل طول، جرم، زمان
+                    double valInBase = 0;
+
+                    // تبدیل به واحد پایه
+                    switch (type)
+                    {
+                        case "طول":
+                            valInBase = value * (from == "m" ? 1 : from == "cm" ? 0.01 : from == "mm" ? 0.001 : from == "km" ? 1000 : from == "ft" ? 0.3048 : 0.0254);
+                            result = valInBase / (to == "m" ? 1 : to == "cm" ? 0.01 : to == "mm" ? 0.001 : to == "km" ? 1000 : to == "ft" ? 0.3048 : 0.0254);
+                            break;
+                        case "جرم":
+                            valInBase = value * (from == "kg" ? 1 : from == "g" ? 0.001 : 0.453592);
+                            result = valInBase / (to == "kg" ? 1 : to == "g" ? 0.001 : 0.453592);
+                            break;
+                        case "زمان":
+                            valInBase = value * (from == "s" ? 1 : from == "min" ? 60 : 3600);
+                            result = valInBase / (to == "s" ? 1 : to == "min" ? 60 : 3600);
+                            break;
+                    }
+                }
+
+                lblResult.Text = result + " " + to;
+            }
+            catch
+            {
+                MessageBox.Show("لطفاً یک عدد صحیح وارد کنید و همه واحدها را انتخاب کنید.");
+            }
+        }
+    }
+}
